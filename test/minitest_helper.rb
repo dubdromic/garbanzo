d2da918e6f17cd40663b1fcd648ee2517c0a9428
Corin Schedler <corin@dubdromic.com>
2015-08-08 17:35:46 -0700

    Fix test account credentials, create test
    In the event testing should be done against the actual Authorize
    test API, set the correct environment variables and comment out
    the stubs you want to work around.

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -6,5 +6,9 @@ require 'minitest/autorun'
 require 'minitest/unit'
 require 'minitest/color'
 
+AUTHORIZE_TEST_LOGIN = ENV['AUTHORIZE_TEST_LOGIN'] || 'fake'
+AUTHORIZE_TEST_KEY = ENV['AUTHORIZE_TEST_KEY'] || 'login'
+
 require 'webmock/minitest'
 WebMock.disable_net_connect!
+
--- test/subscription/test_create.rb
+++ test/subscription/test_create.rb
@@ -5,12 +5,35 @@ module Garbanzo
     def setup
       @klass = Garbanzo::Subscription::Create
       stub_request(:post, 'https://apitest.authorize.net/xml/v1/request.api')
+        .to_return(body: success_body)
+    end
+
+    def success_body
+      <<-BODY
+<?xml version="1.0" encoding="utf-8"?>
+<ARBCreateSubscriptionResponse
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+  xmlns="AnetApi/xml/v1/schema/AnetApiSchema.xsd">
+  <messages>
+    <resultCode>Ok</resultCode>
+    <message>
+      <code>I00001</code>
+      <text>Successful.</text>
+    </message>
+  </messages>
+  <subscriptionId>2788088</subscriptionId>
+</ARBCreateSubscriptionResponse>
+BODY
     end
 
     def test_general_function
-      credentials = Garbanzo::Credentials.new('name', 'pw')
-      response = @klass.new(credentials).create(0, {}, {}, Duration.new, Interval.new)
-      assert_equal response, { id: -1, code: '', message: '' }
+      credentials = Garbanzo::Credentials.new(AUTHORIZE_TEST_LOGIN, AUTHORIZE_TEST_KEY)
+      card = { card_number: '4111111111111111', expiration_date: '2020-06' }
+      address = { first_name: 'First', last_name: 'Last' }
+      response = @klass
+        .new(credentials).create(1, card, address, Duration.new(Date.today + 2), Interval.new)
+      assert_equal response, { id: 2788088 }
     end
   end
 end
