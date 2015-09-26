0e50d0508070deba44068177afac951926132ffe
Corin Schedler <corin@dubdromic.com>
2015-09-25 23:45:52 -0700

    Add ENV switch to stub Authorize requests

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -5,15 +5,21 @@ require 'garbanzo'
 require 'minitest/autorun'
 require 'minitest/unit'
 require 'minitest/color'
+require 'webmock/minitest'
 
 AUTHORIZE_TEST_LOGIN = ENV['AUTHORIZE_TEST_LOGIN'] || 'fake'
 AUTHORIZE_TEST_KEY = ENV['AUTHORIZE_TEST_KEY'] || 'login'
+USE_REQUEST_STUBS = ENV['STUB_REQUESTS'] || AUTHORIZE_TEST_LOGIN == 'fake'
 
-require 'webmock/minitest'
-WebMock.disable_net_connect!
+if USE_REQUEST_STUBS
+  WebMock.disable_net_connect!
+else
+  WebMock.allow_net_connect!
+end
 
 module AuthorizeStubs
   def stub_authorize_request(body)
+    return unless USE_REQUEST_STUBS
     stub_request(:post, 'https://apitest.authorize.net/xml/v1/request.api')
       .to_return(body: body)
   end
